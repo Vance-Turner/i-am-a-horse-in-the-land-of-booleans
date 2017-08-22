1abe993eec772848fef0650015644a2176e9bd4e
Vance Turnewitsch <vance.turner1@gmail.com>
2017-08-22 10:40:00 -0400

    Started working, up to exercise 6

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,19 +2,41 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if (x)
+    (true)
+    (false))
+  )
 
 (defn abs [x]
-  ":(")
+  (if (< x 0)
+    (* -1 x)
+    x
+    )
+  )
 
 (defn divides? [divisor n]
-  ":(")
+  (if (== (mod n divisor) 0)
+    true
+    false
+    )
+  )
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+    (divides? 15 n) "gotcha!"
+    (divides? 3 n)  "fizz"
+    (divides? 5 n)  "buzz"
+    :else           ""
+     )
+  )
 
 (defn teen? [age]
-  ":(")
+  (if (<= 13 age 19)
+    (true)
+    (false)
+    )
+  )
+
 
 (defn not-teen? [age]
   ":(")
